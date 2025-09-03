3dc0a57263d64e755be7201082b16356bb7897ed
Thushini Akashi <thushiniakashi58@gmail.com>
2025-09-04 03:29:04 +0530

    add main tsk and update that code

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from '../router.tsx';
+import { Toaster } from "react-hot-toast";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <>
+            <RouterProvider router={router} />
+            <Toaster position="top-right" reverseOrder={false} />
+        </>
+    </StrictMode>
 );

81b4d773b0398abe73c9db1e952c983f0cc216a2
Thushini Akashi <thushiniakashi58@gmail.com>
2025-09-04 03:35:18 +0530

    remove some tsk file and get it to one file

--- src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="py-6 text-center border-t dark:border-white/10 mt-10">
-            <p className="text-sm text-gray-600 dark:text-gray-400">
-                © {new Date().getFullYear()} Thushini Akashi. All rights reserved.
-            </p>
-        </footer>
-    );
-}

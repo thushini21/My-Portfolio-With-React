ee10f88867f24b1cfdd140525cfd61cc390ae790
Thushini Akashi <thushiniakashi58@gmail.com>
2025-09-04 09:02:04 +0530

    update main tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+// @ts-ignore
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from '../router.tsx';

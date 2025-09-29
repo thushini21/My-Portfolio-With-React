c6d507093070020f713684bea3b3c5e7d9abd5d0
Thushini Akashi <thushiniakashi58@gmail.com>
2025-09-29 11:51:23 +0530

    update home page

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,9 @@
 import {Outlet} from "react-router-dom"
-import Homepage from "./Homepage";
 
 const Layout = (): any => {
     return (
         <div className='h-screen'>
-            <div className='fixed top-0 left-0 right-0 z-50'>
-                <Homepage/>
-            </div>
-            <main className='pt-16 h-full overflow-y-auto'>
+            <main className='h-full overflow-y-auto'>
                 <Outlet/>
             </main>
         </div>

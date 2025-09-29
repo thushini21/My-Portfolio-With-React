d6c0c6eaa6bd88920d0eb76bb09b037a3144bd41
Thushini Akashi <thushiniakashi58@gmail.com>
2025-09-29 11:49:47 +0530

    add strick mode and import index.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,12 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// @ts-ignore
-import "./index.css";
+import './index.css';
 import { RouterProvider } from "react-router-dom";
 import router from '../router.tsx';
 import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-        <>
-            <RouterProvider router={router} />
-            <Toaster position="top-right" reverseOrder={false} />
-        </>
-    </StrictMode>
+    <>
+        <RouterProvider router={router} />
+        <Toaster position="top-right" reverseOrder={false} />
+    </>
 );
